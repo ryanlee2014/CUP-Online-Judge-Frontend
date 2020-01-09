09eb7568a946d1ff78457dc561153cfd0dcdcf82
Ryan Lee <gxlhybh@gmail.com>
2020-01-09 10:31:31 +0800

    [Fix] Release 1.4.4 Ignore contest mode when user is admin

--- src/store/system/getters.ts
+++ src/store/system/getters.ts
@@ -1,8 +1,9 @@
 import { GetterTree } from "vuex";
 import { ISystemState } from "@/store/system/system";
 import { IRootState } from "@/store/root";
+import store from "@/store";
 const getters: GetterTree<ISystemState, IRootState> = {
-    contestMode: state => state.contestMode
+    contestMode: state => !store.getters.admin && state.contestMode
 };
 
 export default getters;
