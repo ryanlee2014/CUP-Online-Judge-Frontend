5be9d6ee0286d4c6482e7bbd39896186852f4211
Ryan Lee <gxlhybh@gmail.com>
2020-02-17 04:05:45 +0800

    update script

--- before_build/preinstall.sh
+++ before_build/preinstall.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd ./semantic/semantic-ui/
+echo 'Curent path:'
+pwd
+if [[ "$OSTYPE" == "darwin"* ]]; then
 sed -i "" "s|@import url('https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin&display=swap')|/* removed */|g" semantic.css
+else
+sed -i "s|@import url('https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin&display=swap')|/* removed */|g" semantic.css
+fi
