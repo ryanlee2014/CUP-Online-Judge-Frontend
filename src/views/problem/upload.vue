bb43e1eb405c0e784045b0ff3206db6d1bdeee3e
Ryan Lee <gxlhybh@gmail.com>
2020-04-27 18:59:20 +0800

    [Release] Release 1.18.15 i18n improved.

--- src/views/problem/upload.vue
+++ src/views/problem/upload.vue
@@ -5,21 +5,21 @@
             <div class="row">
                 <div class="ui center aligned segment" style="min-height:400px">
                     <div class="ui main container">
-                        <form action="/api/upload/user" class="ui form" enctype="multipart/form-data" method="post">
-                            <h3>Import Problem(RPK):</h3>
+                        <form ref="upload_form" class="ui form">
+                            <h3>{{$t("import problem")}}</h3>
                             <div class="field">
                                 <a @click="selectFile" class="ui button">{{fileStatus}}</a>
                                 <input @change="fileChange" ref="file" id="file" name="fps" style="display:none" type="file">
                             </div>
                             <div class="two field">
                                 <div class="ui left input" style="width: auto;"><input id="vcode" name="captcha"
-                                                                                       placeholder="验证码" type="text"><img
+                                                                                       :placeholder="$t('captcha')" type="text"><img
                                         alt="click to change" height="40px" id="vcode_graph"
                                         onclick="this.src='/api/captcha?from=upload&random='+Math.random()"
                                         :src="`/api/captcha?from=upload&random=${captchaHash}`"></div>
                             </div>
                             <div class="field">
-                                <input class="ui blue button" type="submit" value="Import">
+                                <a @click="uploadFile" class="ui primary button">{{$t("submit")}}</a>
                             </div>
                         </form>
                     </div>
@@ -28,7 +28,19 @@
         </div>
     </div>
 </template>
-
+<i18n>
+    {
+        "zh-cn": {
+            "import problem": "导入题目RPK文件"
+        },
+        "en": {
+            "import problem": "Import Problem RPK File"
+        },
+        "ja": {
+            "import problem": "インポートファイル"
+        }
+    }
+</i18n>
 <script lang="ts">
 import mixins from "../../mixin/init";
 import { Component, Mixins } from "vue-property-decorator";
@@ -37,9 +49,6 @@ import CaptchaMixin from "@/mixin/CaptchaMixin";
 @Component
 export default class ProblemUpload extends Mixins(mixins, CaptchaMixin) {
     fileStatus = "选择文件";
-    $refs!: {
-        file: HTMLFormElement
-    };
 
     mounted () {
         document.title = `Upload problem -- ${document.title}`;
@@ -50,7 +59,21 @@ export default class ProblemUpload extends Mixins(mixins, CaptchaMixin) {
     }
 
     selectFile () {
-        this.$refs.file.click();
+        (this.$refs.file as HTMLElement).click();
+    }
+
+    uploadFile () {
+        const data = new FormData((this.$refs as any).upload_form as any);
+        const config = {
+            onUploadProgress: function (progressEvent: any) {
+                const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                console.log(percentCompleted);
+            }
+        };
+        this.axios.post("/api/upload/user", data, config)
+            .then(({ data }) => {
+                alert("上传成功");
+            });
     }
 }
 </script>
