652495b1e127d6bdb54f05fc0714db372096a0fe
Ryan Lee <gxlhybh@gmail.com>
2020-06-25 00:03:33 +0800

    [Release] Release 1.24.6 Contest Topic UI Friendly.

--- src/views/contesttopic/contest-topic-view.vue
+++ src/views/contesttopic/contest-topic-view.vue
@@ -6,10 +6,16 @@
         <table class="ui padded borderless unstackable selectable table">
             <thead>
                 <tr>
-                    <th width="55%">
+                    <th>
                         {{$t("title")}}
                     </th>
-                    <th width="25%">
+                    <th v-if="admin" width="7%">
+                        {{$t("visible")}}
+                    </th>
+                    <th v-if="admin" width="7%">
+                        {{$t("enable")}}
+                    </th>
+                    <th width="15%">
                         {{$t("creator")}}
                     </th>
                     <th width="20%">
@@ -18,11 +24,17 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="row in contestTopicList" :key="row.contestset_id">
+                <tr v-for="row in contestTopicList" :key="row.contestset_id" :class="isVisible(row)">
                     <td>
                         <router-link :to="`/topic/${row.contestset_id}/`">
                             {{row.title}}
                         </router-link>
+                    </td>
+                    <td v-if="admin">
+                        {{ row.visible ? $t("visible") : $t("hide") }}
+                    </td>
+                    <td v-if="admin">
+                        {{ row.defunct === "N" ? $t("enable") : $t("disable") }}
                     </td>
                     <td>
                         <router-link :to="`/user/${row.creator}`">
@@ -41,8 +53,35 @@ import { Component, Mixins } from "vue-property-decorator";
 import InitMixin from "@/mixin/init";
 import { IContestSetResponseDTO } from "@/types/contestset";
 import dayjs from "dayjs";
+import { mapGetters } from "vuex";
 
-@Component
+@Component({
+    computed: {
+        ...mapGetters(["admin"])
+    },
+    i18n: {
+        messages: {
+            "zh-cn": {
+                visible: "可见",
+                hide: "隐藏",
+                enable: "启用",
+                disable: "停用"
+            },
+            en: {
+                visible: "Visible",
+                hide: "Invisible",
+                enable: "Enabled",
+                disable: "Disabled"
+            },
+            ja: {
+                visible: "見える",
+                hide: "隠れる",
+                enable: "有効",
+                disable: "無効"
+            }
+        }
+    }
+})
 export default class ContestTopicView extends Mixins(InitMixin) {
     dayjs = dayjs;
     contestTopicList: IContestSetResponseDTO[] = [];
@@ -50,6 +89,10 @@ export default class ContestTopicView extends Mixins(InitMixin) {
 
     mounted () {
         this.initData();
+    }
+
+    isVisible (row: any) {
+        return !row.visible ? "active" : "";
     }
 
     initData () {
